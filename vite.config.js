d11b97bd471271ed725ab25a38ed31ba396c5f12
JeanCarlos Jimenez <jeancarlosjimenezc25@gmail.com>
2024-06-04 14:03:32 -0500

    Changes Projects - Linux

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -6,4 +7,3 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
 })
-
